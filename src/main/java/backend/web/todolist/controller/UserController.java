6d2161eb418a5d0e18a80c337e909f1af0795e82
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-06-13 11:34:07 -0300

    PostMapping OK - UserService OK

--- src/main/java/backend/web/todolist/controller/UserController.java
+++ src/main/java/backend/web/todolist/controller/UserController.java
@@ -2,17 +2,29 @@ package backend.web.todolist.controller;
 
 import backend.web.todolist.controller.dto.CreateUserDto;
 import backend.web.todolist.entities.User;
+import backend.web.todolist.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
 
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody CreateUserDto dto){
-        return null;
+        var userId = userService.createUser(dto);
+        return ResponseEntity.created(URI.create("/v1/users" + userId.toString())).build();
     }
+
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable("userId") String userId){
         return null;
---
+++ src/main/java/backend/web/todolist/service/UserService.java
@@ -0,0 +1,38 @@
+package backend.web.todolist.service;
+
+import backend.web.todolist.controller.dto.CreateUserDto;
+import backend.web.todolist.entities.User;
+import backend.web.todolist.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.UUID;
+
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public UUID createUser(CreateUserDto createUserDto){
+
+        // DTO -> Entity
+        var entity = new User(
+                UUID.randomUUID(),
+                createUserDto.username(),
+                createUserDto.email(),
+                createUserDto.password(),
+                Instant.now(),
+                null
+        );
+
+        var userSaved = userRepository.save(entity);
+
+        return userSaved.getUserid();
+    }
+}
