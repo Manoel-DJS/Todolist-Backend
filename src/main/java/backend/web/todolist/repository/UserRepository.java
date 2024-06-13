8e376a48980680132ce533f2869a414686e53622
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-06-13 09:24:06 -0300

    create package repository and interface userRepo

---
+++ src/main/java/backend/web/todolist/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package backend.web.todolist.repository;
+
+import backend.web.todolist.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
