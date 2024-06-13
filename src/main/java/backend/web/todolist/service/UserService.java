ffd73c3746d6e93aca1b376f9f4c35bef9bfa49f
Manoel-DJS <94149934+Manoel-DJS@users.noreply.github.com>
2024-06-13 12:22:43 -0300

    Create GetMapping for list Users

--- src/main/java/backend/web/todolist/service/UserService.java
+++ src/main/java/backend/web/todolist/service/UserService.java
@@ -6,6 +6,7 @@ import backend.web.todolist.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -39,6 +40,10 @@ public class UserService {
 
     public Optional<User> getUserById(String userId){
         return userRepository.findById(UUID.fromString(userId));
+    }
+
+    public List<User> listUsers(){
+        return userRepository.findAll();
     }
 
 }
